7ae42c539166fd25a3b951869d06544bc2d26c8e
waleed <waleedkaleed24@gmail.com>
2024-12-22 23:53:07 +0100

    refactor routes and remove unused password confirmation, reset, and update tests

--- routes/web.php
+++ routes/web.php
@@ -4,34 +4,28 @@ use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\ReminderController;
-use App\Models\Appointment;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $appointments = [];
-    if (auth()->check()){
-        $appointments = auth()->user()->userAppointments()->latest()->get();
-    }
+    $appointments = auth()->check() ? auth()->user()->userAppointments()->latest()->get() : [];
     return view('home', ['appointments' => $appointments]);
-
 })->name('home');
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
     Route::post('register', [RegisteredUserController::class, 'store']);
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 });
 
 Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
+// Appointment routes
+Route::prefix('appointment')->group(function () {
+    Route::post('create', [AppointmentController::class, 'createAppointment']);
+    Route::put('edit/{appointment}', [AppointmentController::class, 'updateAppointment']);
+    Route::delete('delete/{appointment}', [AppointmentController::class, 'deleteAppointment']);
+});
 
-// appointment
-Route::post('/create-appointment', [AppointmentController::class, 'createAppointment']);
-Route::put('/edit-appointment/{appointment}', [AppointmentController::class, 'updateAppointment']);
-Route::delete('/delete-appointment/{appointment}', [AppointmentController::class, 'deleteAppointment']);
-
-
-// Test email
-Route::get('/send-reminder', [ReminderController::class, 'sendReminder']);
+// Test email route
+Route::get('send-reminder', [ReminderController::class, 'sendReminder']);
